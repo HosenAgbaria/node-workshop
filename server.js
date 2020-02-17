df9602b13e868580d696a79afcd1aa3595cddd73
HosenAgbaria <hosen.agbaria@hotmail.com>
2020-02-18 01:56:57 +0200

    step 4 read file and returning it inside the handler

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
-const http = require('http')
-const port = 3000
-var message = 'I am so happy to be part of the Node Girls workshop!';
+const http = require("http");
+const port = 3000;
+var message = "I am so happy to be part of the Node Girls workshop!";
 
-let handler =(req,res) =>{
-    
-    res.writeHead(200,{'content-type' : 'text/html'})
-    res.write(message)
-    res.end()
-}
-const server = http.createServer(handler)
+const fs = require("fs");
 
-server.listen(port ,()=>{
-    console.log(`server is running successfully on localhost:${ port } and ready to get requests `);
-    
-})
+let handler = (req, res) => {
+  let reqUrl = req.url;
+
+  if (reqUrl === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    fs.readFile(__dirname + "/public/index.html", (err, file) => {
+      if (err) {
+        console.log(err);
+        return;
+      } else {
+        res.end(file);
+      }
+    });
+  } else if (reqUrl === "/node") {
+    res.writeHead(200, { "content-type": "text/html" });
+
+    res.write("you requseted node page");
+    res.end();
+  } else if (reqUrl === "/girls") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("girls end point");
+    res.end();
+  } else {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(message);
+    res.end();
+  }
+};
+const server = http.createServer(handler);
+
+server.listen(port, () => {
+  console.log(
+    `server is running successfully on localhost:${port} and ready to get requests `
+  );
+});
